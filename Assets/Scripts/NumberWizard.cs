af03aa24bb1ef51568e35f3f8a0264be46520f15
Saiko <a.m.abdelmottaleb@gmail.com>
2018-11-25 14:25:42 +0200

    Code Refactoring for UI Functionality

--- Assets/Scripts/NumberWizard.cs
+++ Assets/Scripts/NumberWizard.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class NumberWizard : MonoBehaviour {
 
     // Use this for initialization
     int max, min, guess;
+    [SerializeField] Text currentGuess;
 	void Start () {
         StartGame();
 	}
@@ -15,31 +16,26 @@ public class NumberWizard : MonoBehaviour {
         max = 1000;
         min = 1;
         guess = 500;
-        Debug.Log("Hello to Number Wizard..");
-        Debug.Log("Pick a number, don't tell me what it is...");
-        Debug.Log("Choose a number between: " + min + " and: " + max);
-        Debug.Log("Tell me if your number is higher or lower than my guess!");
-        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
         max += 1;
     }
 
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            min = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            max = guess;
-            NextGuess();
-        }
-        else if (Input.GetKeyDown(KeyCode.Return))
-        {
-            Debug.Log("I am a genius.");
-            StartGame();
-        }
+        currentGuess.text = guess.ToString();
 	}
+
+    public void HigherGuess() {
+        min = guess;
+        NextGuess();
+    }
+    public void LowerGuess() {
+        max = guess;
+        NextGuess();
+    }
+
+    public void CorrectAnswer() {
+        Debug.Log("I am a genius.");
+        StartGame();
+    }
 
     void NextGuess()
     { 
